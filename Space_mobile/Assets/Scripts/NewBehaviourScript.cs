ae943b38763c78793be83458c355c80e4ee24758
RedSpiderP5 <106278435+RedSpiderP5@users.noreply.github.com>
2022-05-27 17:47:55 +0300

    Хоба

---
+++ Space_mobile/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public bool building;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnMouseEnter()
+    {
+        if (building == true)
+        {
+            transform.GetChild(0).GetComponent<Image>().color = new Color(50/255.0f, 200/255.0f, 50/255.0f, 150/255.0f);
+        }
+        else
+        {
+            transform.GetChild(0).GetComponent<Image>().color = new Color(200/255.0f, 50/255.0f, 50/255.0f, 150/255.0f);
+        }
+
+    }
+
+    void OnMouseExit()
+    {
+        transform.GetChild(0).GetComponent<Image>().color = new Color(100/255.0f, 100/255.0f, 100/255.0f, 200/255.0f);
+    }
+}
