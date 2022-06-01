90dd7a641071b2190b9498303f658cb47706ac2c
UnrealStephan <86574529+UnrealStephan@users.noreply.github.com>
2022-06-01 19:49:18 +0300

    Shop

--- Space_mobile/Assets/Scripts/Backlight.cs
+++ Space_mobile/Assets/Scripts/Backlight.cs
@@ -6,33 +6,30 @@ using UnityEngine.UI;
 public class Backlight : MonoBehaviour
 {
 
-    public New NW;
-    public GameObject ShopCell;
+    public bool Busy = false;
+    public GameObject Shop;
+    public GameObject Map;
 
     void Start()
     {
-        
     }
 
     void Update()
     {
-
     }
 
     private void OnMouseDown()
     {
-        NW.Map.SetActive(false);
-        transform.GetChild(0).GetChild(0).GetComponent<Image>().color = new Color(255, 255, 255, 0.0f);
-        ShopCell.GetComponentInChildren<Outline>().effectColor = Color.black;
-    }
-
-    void OnMouseEnter()
-    {
-        transform.GetChild(0).GetChild(0).GetComponent<Image>().color = new Color(255, 255, 255, 0.03f);
-    }
-
-    void OnMouseExit()
-    {
-        transform.GetChild(0).GetChild(0).GetComponent<Image>().color = new Color(255, 255, 255, 0.0f);
+        if (Busy == true)
+        {
+            return;
+        }
+        else
+        {
+            Instantiate(Shop.transform.GetComponent<New_spaceship>().Active_ship).transform.position = transform.GetChild(5).transform.position;
+            Busy = true;
+            Map.SetActive(false);
+            Shop.transform.GetComponent<New_spaceship>().Outline_removal();
+        }
     }
 }
